634ec4c63ad5d73968b3821f2c903baf927fde4b
Yuri <yura.mezin@gmail.com>
2018-07-02 15:47:53 +1000

    оптимизация

--- lumentest/routes/web.php
+++ lumentest/routes/web.php
@@ -1,21 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
+// homepage
 $router->get('/', function () use ($router) {
-    $oLogs = new App\Logs();
-    $info = $oLogs->info();
-    $oPDB = new App\PDB();
-    $dbInfo = $oPDB->dbInfo();
+    $info = (new App\Logs)->info();
+    $dbInfo = (new App\PDB)->dbInfo();
 
     return view('home', compact('info', 'dbInfo'));
 });
